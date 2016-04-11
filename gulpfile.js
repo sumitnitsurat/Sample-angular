350f1a5d05439481433f4c3852287deb08bae950
sumit chaudhari <sumit.nitsurat@gmail.com>
2016-04-12 00:56:07 +0530

    - Package.json, gulpfile and index.html modfied

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var csslint = require('gulp-csslint');
+var bootlint = require('gulp-bootlint');
+
 
 var nodemon = require('gulp-nodemon');
 //var connect = require('gulp-connect');
@@ -41,11 +48,23 @@ gulp.task('copyfiles', function() {
     gulp.src('index.html')
         .pipe(gulp.dest('./public/'));
 		
-	 gulp.src('./src/assets/js/*.js')
-        .pipe(gulp.dest('./public/js/'));
+	 gulp.src('./dist/**/*.*')
+        .pipe(gulp.dest('./public/dist/'));
 
-    gulp.src('./src/assets/css/*.css')
-        .pipe(gulp.dest('./public/css/'));	
+    gulp.src('./build/**/*.*')
+        .pipe(gulp.dest('./public/build/'));
+	
+	gulp.src('./bootstrap/**/*.*')
+        .pipe(gulp.dest('./public/bootstrap/'));
+		
+	gulp.src('./pages/**/*.*')
+        .pipe(gulp.dest('./public/pages/'));
+
+	gulp.src('./plugins/**/*.*')
+        .pipe(gulp.dest('./public/plugins/'));	
+
+	gulp.src('./documentation/**/*.*')
+        .pipe(gulp.dest('./public/documentation/'));		
 
     //gulp.src('./src/app/charts/*.js')
     //    .pipe(gulp.dest('./public/js/charts/'));
@@ -62,6 +81,86 @@ gulp.task('copyfiles', function() {
     gulp.src('./src/app/json/api/v1/**/*.json')
         .pipe(gulp.dest('./public/json/'));
 });
+
+/*..... admin LTE tasks ...*/
+
+
+
+gulp.task('watch', function () {
+  gulp.watch('build/less/*.less,build/less/skins/*.less,dist/js/app.js', [ /* dependencies */ ]);
+});
+
+gulp.task('watch', function () {
+  gulp.watch('less,uglify', [ /* dependencies */ ]);
+});
+
+gulp.task('less', function () {
+  return gulp
+    .src('build/less/AdminLTE.less')
+    .pipe(gulp.dest('dist/css/AdminLTE.css,dist/css/skins/skin-blue.css,dist/css/skins/skin-black.css,dist/css/skins/skin-yellow.css,dist/css/skins/skin-green.css,dist/css/skins/skin-red.css,dist/css/skins/skin-purple.css,dist/css/skins/skin-blue-light.css,dist/css/skins/skin-black-light.css,dist/css/skins/skin-yellow-light.css,dist/css/skins/skin-green-light.css,dist/css/skins/skin-red-light.css,dist/css/skins/skin-purple-light.css,dist/css/skins/_all-skins.css'))
+  ;
+});
+
+
+gulp.task('uglify', function () {
+  return gulp
+    .src('undefined')
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'))
+  ;
+});
+
+gulp.task('includes', function () {
+  return gulp
+    .src('*.html')
+    .pipe(gulp.dest('documentation/'))
+  ;
+});
+
+gulp.task('jshint', function () {
+  return gulp
+    .src('dist/js/app.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+  ;
+});
+
+gulp.task('jshint', function () { // WARNING: potential duplicate task
+  return gulp
+    .src('dist/js/demo.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+  ;
+});
+
+gulp.task('jshint', function () { // WARNING: potential duplicate task
+  return gulp
+    .src('dist/js/pages/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+  ;
+});
+
+gulp.task('csslint', function () {
+  return gulp
+    .src('dist/css/AdminLTE.css')
+    .pipe(gulp.dest('dist'))
+  ;
+});
+
+gulp.task('bootlint', function () {
+  return gulp
+    .src('pages/**/*.html,*.html')
+  ;
+});
+
+
+gulp.task('lint', ["jshint","csslint","bootlint"]);
+
+
+/*......Admin Lte tasks endds.....*/
+
 
 gulp.task('build', [ 'copyfiles' ,'connect','browserify','watch', 'watch-less' ]);
 gulp.task('cd', [ 'copyfiles' ,'browserify' ]);
